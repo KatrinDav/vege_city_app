e6522c047239c36d89a3daad06cb0d46c3aa4f50
Katrin Dav <k.dawidowicz2016@gmail.com>
2021-12-05 18:08:03 +0100

    add fetching data from backend

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,54 +1,83 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ItemMenu from './ItemMenu/ItemMenu';
 import Title from '../Title/Title';
+import Loader from '../Loader/Loader';
 import styles from './Menu.module.scss';
 
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Vege Ramen',
-        description: 'Creamy konbu ramen with shiitake mushrooms, edamame, pak choi, baked sweet potato, tofu, sprouts',
-        price: 12.99,
-    },
-    {
-        id: 'm2',
-        name: 'Hummus with tomatoes',
-        description: 'Our speciality with dried tomatoes',
-        price: 16.59,
-    },
-    {
-        id: 'm3',
-        name: 'Vegan Wrap',
-        description: 'Marinated seitan, lettuce, cucumber, tomato, and pepper',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Cream sweet potato',
-        description: 'Baked sweet potatoes, coconut milk, and ginger',
-        price: 18.99,
-    },
-    {
-        id: 'm5',
-        name: 'Spaghetti Bolognese',
-        description: 'Soy spaghetti bolognese in tomato sauce with vegan parmesan and cherry tomatoes',
-        price: 15.25,
-    },
-    {
-        id: 'm6',
-        name: 'Noodle Teriyaki',
-        description: 'Udon noodles with sprouts, mun mushrooms, edamame, tofu, carrots, red onion, sugar peas, and sesame',
-        price: 17.65,
-    },
-];
+// const DUMMY_MEALS = [
+//     {
+//         id: 'm1',
+//         name: 'Vege Ramen',
+//         description: 'Creamy konbu ramen with shiitake mushrooms, edamame, pak choi, baked sweet potato, tofu, sprouts',
+//         price: 12.99,
+//     },
+//     {
+//         id: 'm2',
+//         name: 'Hummus with tomatoes',
+//         description: 'Our speciality with dried tomatoes',
+//         price: 16.59,
+//     },
+//     {
+//         id: 'm3',
+//         name: 'Vegan Wrap',
+//         description: 'Marinated seitan, lettuce, cucumber, tomato, and pepper',
+//         price: 12.95,
+//     },
+//     {
+//         id: 'm4',
+//         name: 'Cream sweet potato',
+//         description: 'Baked sweet potatoes, coconut milk and ginger',
+//         price: 18.99,
+//     },
+//     {
+//         id: 'm5',
+//         name: 'Spaghetti Bolognese',
+//         description: 'Soy spaghetti bolognese in tomato sauce with vegan parmesan and cherry tomatoes',
+//         price: 15.25,
+//     },
+//     {
+//         id: 'm6',
+//         name: 'Noodle Teriyaki',
+//         description: 'Udon noodles with sprouts, mun mushrooms, edamame, tofu, carrots, red onion, sugar peas and sesame',
+//         price: 17.65,
+//     },
+// ];
+
+const API_URL = 'https://vege-city-app-default-rtdb.europe-west1.firebasedatabase.app/meals.json';
 
 const Menu = () => {
+    const [menu, setMenu] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        fetch(API_URL)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                const loadedMeals = [];
+                for (const key in data) {
+                    loadedMeals.push({
+                        id: key,
+                        name: data[key].name,
+                        description: data[key].description,
+                        price: data[key].price
+                    })
+                }
+                setMenu(loadedMeals);
+            })
+        setIsLoading(false)
+
+    }, []);
+
+
     return (
         <>
             <Title sbTitle='Menu' title='Specials of the week' />
-            <section className={styles.menu_container}>
 
-                {DUMMY_MEALS.map(item => (
+            <section className={styles.menu_container}>
+                {isLoading && <Loader />}
+                {menu.map(item => (
                     <ItemMenu
                         key={item.id}
                         id={item.id}
@@ -56,9 +85,7 @@ const Menu = () => {
                         description={item.description}
                         price={item.price}
                     />
-                ))
-
-                }
+                ))}
             </section>
         </>
     );
