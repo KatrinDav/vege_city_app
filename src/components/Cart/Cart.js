d45c8a385866d9a5b9157f758b09198fce7bb1c2
Katrin Dav <k.dawidowicz2016@gmail.com>
2021-12-14 21:51:10 +0100

    add fetch orders

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,8 +6,12 @@ import CartItem from './CartItem/CartItem';
 import Form from './Form/Form';
 import styles from './Cart.module.scss';
 
+
+const ORDERS_API = 'https://vege-city-app-default-rtdb.europe-west1.firebasedatabase.app/orders.json';
+
 const Cart = ({ hideCart }) => {
     const [isFormVisible, setIsFormVisible] = useState(false);
+    const [didSubmit, setDidSubmit] = useState(false);
     const context = useContext(CartContext);
     const total = context.totalAmount.toFixed(2);
 
@@ -25,33 +29,67 @@ const Cart = ({ hideCart }) => {
         setIsFormVisible(true)
     }
 
-    return (
-        <Modal>
-            <ul className={styles.cart_items}>
-                {context.items.map((item => (
-                    <CartItem
-                        key={item.id}
-                        name={item.name}
-                        price={item.price}
-                        amount={item.amount}
-                        onRemove={() => removeItemHandler(item.id)}
-                        onAdd={() => addItemHandler(item)} />
-                )))}
-            </ul>
-            <div className={styles.total}>
-                <span>Total Amount</span>
-                <span> $ {total}</span>
-            </div>
-            {isFormVisible && <Form onClose={hideCart} />}
+    const confirmOrder = async (userData) => {
+        await fetch(ORDERS_API, {
+            method: 'POST',
+            body: JSON.stringify({
+                user: userData,
+                order: context.items
+            })
+        })
+        context.clearCart()
+        setIsFormVisible(false)
+        setDidSubmit(true)
+    }
 
-            {!isFormVisible && (
+    const SubmitMessage = () => {
+        return (
+            <>
+                <h2 className={styles.message}>Your order has been send! :)</h2>
                 <div className={styles.actions}>
                     <Button clickFn={hideCart} type='button'>Close</Button>
-                    {hasItems && <Button clickFn={showForm} type='button'>Order</Button>}
+
                 </div>
-            )}
+            </>
+        )
+    }
 
 
+    const ModalContent = () => {
+        return (
+            <>
+                <ul className={styles.cart_items}>
+                    {context.items.map((item => (
+                        <CartItem
+                            key={item.id}
+                            name={item.name}
+                            price={item.price}
+                            amount={item.amount}
+                            onRemove={() => removeItemHandler(item.id)}
+                            onAdd={() => addItemHandler(item)} />
+                    )))}
+                </ul>
+                <div className={styles.total}>
+                    <span>Total Amount</span>
+                    <span> $ {total}</span>
+                </div>
+                {isFormVisible && <Form onClose={hideCart} onConfirm={confirmOrder} />}
+
+                {!isFormVisible && (
+                    <div className={styles.actions}>
+                        <Button clickFn={hideCart} type='button'>Close</Button>
+                        {hasItems && <Button clickFn={showForm} type='button'>Order</Button>}
+                    </div>
+                )}
+
+            </>
+        )
+    }
+
+    return (
+        <Modal>
+            {didSubmit && <SubmitMessage />}
+            {!didSubmit && <ModalContent />}
         </Modal>
     );
 }
