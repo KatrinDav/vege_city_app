68aa6434aba2ade647c48201d99738a639363c01
Katrin Dav <k.dawidowicz2016@gmail.com>
2022-01-23 12:39:45 +0100

    import and implement Aos librabry

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import CartProvider from './store/CartProvider';
 import About from './components/About/About';
 import Header from './components/Header/Header';
@@ -7,6 +7,8 @@ import Cart from './components/Cart/Cart';
 import Offers from './components/Offers/Offers';
 import Menu from './components/Menu/Menu';
 import Footer from './components/Footer/Footer';
+import Aos from 'aos';
+import "aos/dist/aos.css";
 
 
 function App() {
@@ -18,15 +20,37 @@ function App() {
   const hideCart = () => {
     setIsCartVisible(false)
   }
+
+  useEffect(() => {
+    Aos.init({
+      duration: 1000,
+    })
+  }, [])
+
+
   return (
     <CartProvider>
       <Header showCart={showCart} />
       {isCartVisible && <Cart hideCart={hideCart} />}
-      <Intro />
-      <About />
-      <Offers />
-      <Menu />
-      <Footer />
+      <div data-aos="fade-down" data-aos-delay="400">
+        <Intro />
+      </div>
+
+      <div data-aos="fade-up">
+        <About />
+      </div>
+
+      <div data-aos="fade-up">
+        <Offers />
+      </div>
+
+      <div data-aos="fade-up">
+        <Menu />
+      </div>
+
+      <div data-aos="fade-up" data-aos-delay="1100">
+        <Footer />
+      </div>
     </CartProvider>
   );
 }
